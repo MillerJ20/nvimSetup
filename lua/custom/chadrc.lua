f53b50d5f9b213f44e1013acda3bd9d841eabbb3
Miller J <miller.johnston17@gmail.com>
2023-10-30 22:48:34 -0700

    Added additional requirements for treesitter

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,7 +1,7 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'chocolate' }
+M.ui = { theme = 'everforest' }
 M.plugins = "custom.plugins"
 M.mappings = require("custom.mappings")
 
--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,5 +1,5 @@
 local options = {
-  ensure_installed = { "lua" },
+  ensure_installed = { "lua, vim, regex, lua, bash, markdown, markdown_inline, html, javascript, cpp" },
 
   highlight = {
     enable = true,
